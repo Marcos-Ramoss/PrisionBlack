1616c78c126d305ead9a095f156f2a1b72b6d07a
Marcos-Ramoss <mar.stark.99@gmail.com>
2025-03-26 08:49:16 -0400

    adcionado logo e ajusta codigo do validator

--- app/middlewares/validator.js
+++ app/middlewares/validator.js
@@ -1,7 +1,7 @@
 const { body, validationResult } = require('express-validator');
 
 const validateDetento = [
-    // Validar nome
+
     body('nome')
         .trim()
         .isLength({ min: 3, max: 100 })
@@ -9,12 +9,10 @@ const validateDetento = [
         .matches(/^[A-Za-zÀ-ú\s]+$/)
         .withMessage('O nome deve conter apenas letras.'),
 
-    // Validar idade
     body('idade')
         .isInt({ min: 18, max: 125 })
         .withMessage('A idade deve estar entre 18 e 125 anos.'),
 
-    // Validar filiação
     body('filiacao')
         .trim()
         .isLength({ min: 3, max: 100 })
@@ -22,14 +20,11 @@ const validateDetento = [
         .matches(/^[A-Za-zÀ-ú\s]+$/)
         .withMessage('A filiação deve conter apenas letras.'),
 
-
-    // Validar crimes
     body('crimes')
         .trim()
         .isLength({ min: 5, max: 500 })
         .withMessage('A descrição dos crimes deve conter entre 5 e 500 caracteres.'),
 
-    // Validar Advogado  
     body('nomeAdvogado')
         .trim()
         .isLength({ min: 3, max: 100 })
@@ -37,7 +32,6 @@ const validateDetento = [
         .matches(/^[A-Za-zÀ-ú\s]+$/)
         .withMessage('O nome deve conter apenas letras.'),
 
-    // Validar AOB    
     body('numeroOAB')
         .trim()
         .isLength({ min: 6, max: 6 })
@@ -45,14 +39,13 @@ const validateDetento = [
         .matches(/^[0-9]+$/)
         .withMessage('O número da OAB deve conter exatamente 6 dígitos.'),
 
-    // Validar Data    
     body('dataVisita')
         .isISO8601()
         .toDate()
         .withMessage('A data de visita tem que ser uma data valida!.')
         .custom((value) => {
             const dataAtual = new Date();
-            dataAtual.setHours(0, 0, 0, 0); // Ajusta a data atual para o início do dia
+            dataAtual.setHours(0, 0, 0, 0);
             if (value < dataAtual) {
                 throw new Error('A data de visita não pode ser no passado.');
             }
@@ -64,8 +57,6 @@ const validateDetento = [
         .isLength({ min: 5, max: 500 })
         .withMessage('A descrição dos crimes deve conter entre 5 e 500 caracteres.'),
 
-
-    // Middleware de erro
     (req, res, next) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
